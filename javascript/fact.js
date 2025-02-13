a51c6bc65ff9be022306c1503c5ed6041371f3d9
Yra ALi <mahboobaliya732@gmail.com>
2025-02-13 20:51:16 +0530

    added js folder

---
+++ javascript/fact.js
@@ -0,0 +1,8 @@
+ let count=1;
+let n=10;
+for(let i=1 ; i<=n ; i++)
+{
+count*= i;
+
+}
+console.log(count);
---
+++ javascript/sixth.js
@@ -0,0 +1,8 @@
+let count = 0;
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 == 0) {
+        count += i;
+        
+    }
+    
+}console.log(count);
